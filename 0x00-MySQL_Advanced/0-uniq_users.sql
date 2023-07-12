fe03cb0d1c9cc284d6b9a8bcc43c80954851a653
attahchisom6 <attahchisom6@gmail.com>
2023-07-12 18:28:52 +0000

    adding

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
---- Creating a table
+-- Write a SQL script that creates a table users
+
 CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREMENT NOT NULL,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	name VARCHAR(255),
-	PRIMARY KEY(id)
+    id int NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
