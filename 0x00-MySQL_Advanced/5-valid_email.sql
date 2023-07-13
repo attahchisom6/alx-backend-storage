4f16715be82c9a02ec424d7a2d1fa539dc1273a9
attahchisom6 <attahchisom6@gmail.com>
2023-07-13 16:23:15 +0000

    added

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,4 @@
--- script that creates a trigger that resets the attribute 'valid_email'
+-- script that creates a trigger that resets the attribute valid_email
 -- only when the email has been changed
 .
 DELIMITER $$
