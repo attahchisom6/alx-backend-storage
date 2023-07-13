7bfeccd4c57c37c57c6a97c20e89d6914a3862a0
attahchisom6 <attahchisom6@gmail.com>
2023-07-13 08:40:26 +0000

    get by style ordered by longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- script that list all band with Glam rock as their main syle ordered by their longevity
 
 select band_name, (IFNULL(split, 2022) - formed) as lifespan FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style = "%Glam rock%"
 ORDER BY lifespan DESC;
