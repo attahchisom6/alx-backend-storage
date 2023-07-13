202fa424224c1810c74d85aaad416898b33fae71
attahchisom6 <attahchisom6@gmail.com>
2023-07-13 15:54:46 +0000

    all

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,9 +5,11 @@ DELIMITER $$
 CREATE TRIGGER update_email_trigger
 BEFORE UPDATE ON users
 FOR EACH ROW
-	BEGIN
-		IF OLD.email != NEW.email THEN
-			SET NEW.valid_email = 0
-		END IF;
-	END $$
-DELIMITER;
+BEGIN
+	IF OLD.email != NEW.email THEN
+			
+		SET NEW.valid_email = 0
+	END IF;
+	
+END $$
+DELIMITER ;
