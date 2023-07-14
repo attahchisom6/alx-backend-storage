6814899d9b9d46f0851821b43c04a36b5f79ddaa
attahchisom6 <attahchisom6@gmail.com>
2023-07-14 23:09:32 +0000

    add

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,9 +1,36 @@
--- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INTEGER)
+-- script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
+
+CREATE OR REPLACE PROCEDURE ComputeAverageWeightedScoreForUser (
+  user_id INTEGER
+)
 BEGIN
-    UPDATE users SET average_score = (SELECT SUM(corrections.score * projects.weight) FROM corrections INNER JOIN projects ON corrections.project_id = projects.id WHERE corrections.user_id = user_id) / (SELECT SUM(projects.weight) FROM corrections INNER JOIN projects ON corrections.project_id = projects.id WHERE corrections.user_id = user_id);
-END
-$$
+  DECLARE data INTEGER;
+  DECLARE weights INTEGER;
+  DECLARE weigted_value INTEGER;
+  DECLARE sum_weight INTEGER;
+  DECLARE weighted_avg INTEGER;
+
+  SELECT score INTO data
+  FROM corrections
+  WHERE user_id = user_id;
+
+  SELECT weight INTO weights
+  FROM projects
+  where id = user_id;
+
+  SELECT SUM(weight) into sum_weight
+  FROM projects
+  WHERE id = user_id;
+
+  SET weighted_value = data * weights
+  IF sum_weights != 0 THEN
+    SET weighted_avg = weighted_value / sum_weight;
+  ELSE
+    SET weighted_avg = 0;
+  END IF;
+
+  UPDATE users
+  SET average_score = weighted_avg
+  WHERE id = user_id;
+END $$
 DELIMITER ;
