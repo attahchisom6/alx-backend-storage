bbea09cecc8010f8aae01322a37e5b9464cd39d6
attahchisom6 <attahchisom6@gmail.com>
2023-07-14 23:42:16 +0000

    procedure for average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,28 +1,23 @@
 -- script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
 
+XELIMITER $$
 CREATE OR REPLACE PROCEDURE ComputeAverageWeightedScoreForUser (
   user_id INTEGER
 )
 BEGIN
-  DECLARE data INTEGER;
   DECLARE weights INTEGER;
-  DECLARE weigted_value INTEGER;
+  DECLARE weigted_value FLOAT;
   DECLARE sum_weight INTEGER;
-  DECLARE weighted_avg INTEGER;
+  DECLARE weighted_avg FLOAT;
 
-  SELECT score INTO data
-  FROM corrections
-  WHERE user_id = user_id;
-
-  SELECT weight INTO weights
-  FROM projects
-  where id = user_id;
+  SELECT SUM(weight * score) INTO weighted_value
+  FROM projects JOIN corrections
+  ON project.id = project_id
+  WHERE user.id = user_id;
 
   SELECT SUM(weight) into sum_weight
   FROM projects
-  WHERE id = user_id;
 
-  SET weighted_value = data * weights
   IF sum_weights != 0 THEN
     SET weighted_avg = weighted_value / sum_weight;
   ELSE
