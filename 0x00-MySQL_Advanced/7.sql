0132ef06066e12741ee51d55ba5bd30b4344e6be
attahchisom6 <attahchisom6@gmail.com>
2023-07-14 20:16:57 +0000

    Creating functions that returns a number division

--- 0x00-MySQL_Advanced/7.sql
+++ 0x00-MySQL_Advanced/7.sql
@@ -1,11 +1,16 @@
-DELIMITER //
+-- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. while noting An average score can be a decimal
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE num_scores INT;
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (
+  IN user_id INTEGER
+  )
+  BEGIN
+    DECLARE total FLOAT;
+    DECLARE student_avg FLOAT;
+    DECLARE num_scores FLOAT;
 
-    SELECT SUM(score) INTO total_score
+    SELECT SUM(score) INTO total
     FROM corrections
     WHERE user_id = user_id;
 
@@ -14,12 +19,13 @@ BEGIN
     WHERE user_id = user_id;
 
     IF num_scores > 0 THEN
-        SET total_score = total_score / num_scores;
+      SET student_avg = total / num_scores;
+    ELSE
+      SET student_avg = 0
     END IF;
 
     UPDATE users
-    SET average_score = total_score
+    SET average_score = student_avg
     WHERE id = user_id;
-END //
-
+END $$
 DELIMITER ;
