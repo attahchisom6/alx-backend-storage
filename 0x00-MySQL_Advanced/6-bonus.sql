670b0992f248b6aa1f7fb2cc238bff02facfeef5
attahchisom6 <attahchisom6@gmail.com>
2023-07-14 08:43:01 +0000

    creating procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,19 +2,19 @@
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS AddBonus;
-CREATE PROCEDURE IF NOT EXISTS AddBonus (
-	IN user_id INT,
+CREATE PROCEDURE AddBonus (
+	IN user_id INTEGER,
 	IN project_name VARCHAR(255),
-	IN score INT
+	IN score INTEGER
 )
 BEGIN
   -- check if the given project_name exist in the projects table
-DECLARE project_id INT;
+DECLARE project_id INTEGER;
 SELECT id INTO project_id FROM projects WHERE name=project_name;
 
 -- if the given project_name does not match any name in projects table name field
 IF project_id IS NULL THEN
-  INSERT INTO projects (name) VALUES (project_name)
+  INSERT INTO projects (name) VALUES (project_name);
   SET project_id = LAST_INSERT_ID();
 END IF;
 INSERT INTO corrections (user_id, project_id, score)
