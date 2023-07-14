aa6b8d44a6405342a8a0afff6d1dd81bf92d51a6
attahchisom6 <attahchisom6@gmail.com>
2023-07-14 13:26:19 +0000

    procedure to encapsulate average student marks

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,26 +6,9 @@ CREATE PROCEDURE ComputeAverageScoreForUser (
   IN user_id INTEGER
   )
   BEGIN
-    DECLARE total FLOAT;
-    DECLARE student_avg FLOAT;
-    DECLARE num_scores FLOAT;
-
-    SELECT SUM(score) INTO total
-    FROM corrections
-    WHERE user_id = user_id;
-
-    SELECT COUNT(*) INTO num_scores
-    FROM corrections
-    WHERE user_id = user_id;
-
-    IF num_scores > 0 THEN
-      SET student_avg = total / num_scores;
-    ELSE
-      SET student_avg = 0
-    END IF;
-
     UPDATE users
-    SET average_score = student_avg
-    WHERE id = user_id;
-END $$
-DELIMITER ;
+    SET average_score = (SELECT AVG(score)
+      FROM corrections
+      WHERE user_id=user_id);
+  END $$
+  DELIMITER ;
