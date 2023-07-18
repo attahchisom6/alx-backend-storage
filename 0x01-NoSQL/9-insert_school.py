0f12bdf3b24c1763d572065b0287b8146a05ae1e
attahchisom6 <attahchisom6@gmail.com>
2023-07-18 07:31:06 +0000

    added

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,5 +8,4 @@ insert_school(mongo_collection, **kwargs):
     """
     function returning the id of an inserted document
     """
-    docmt = mongo_collection.insert_one(kwargs)
-    return docmt.inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
