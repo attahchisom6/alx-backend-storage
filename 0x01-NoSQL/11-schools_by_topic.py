d1df29353da363d7a2aed6e12737654e4ee15709
attahchisom6 <attahchisom6@gmail.com>
2023-07-18 08:16:04 -0700

    school by topic'

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -12,8 +12,5 @@ def schools_by_topic(mongo_collection, topic):
         mongo_collection: this will be the pymongo collection object
         topic (string): this will be topic searched
     """
-    schools_by_topic = []
     cursor = mongo_collection.find({"topics": topic})
-    for school in cursor:
-        schools_by_topic.append(school)
-    return schools_by_topic
+    return cursor
