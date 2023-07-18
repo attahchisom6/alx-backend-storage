5a52117591fba6ad712eb75e03575177498db99c
attahchisom6 <attahchisom6@gmail.com>
2023-07-18 14:44:48 +0000

    get schools by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function to return a list of school that studies a given topic
+
+    Args:
+        mongo_collection: this will be the pymongo collection object
+        topic (string): this will be topic searched
+    """
+    return mongo_collection.find({"topic": topic})
