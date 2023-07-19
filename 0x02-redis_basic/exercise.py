c236d433b404c2ebd054df25d6dccce3fd3d5aec
attahchisom6 <attahchisom6@gmail.com>
2023-07-19 14:59:53 +0000

    excersing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ module to create a redis  class
 """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(key: str, fn: Callable[None]) -> UnionOfTypes:
+        if key is None:
+            return None
+        def fn(key: str) -> UnionOfTypes:
+            """
+            return a converted data
+            """
+            return key.converted()
+        return fn(key)
+
+    def get_str(self):
+        """
+        parameterize get to str
+        """
+        return str(Cache.get())
+
+    def get_int(self):
+        """
+        parameterize the return of get into int
+        """
+        return int(cache.get())
